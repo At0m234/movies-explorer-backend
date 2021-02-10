5557abcba63a738aaa8454e8fcd2fff0f73b5541
At0m234 <61670155+At0m234@users.noreply.github.com>
2021-02-10 17:58:27 +0300

    2 stage backend

--- middlewares/centralized-error-handler.js
+++ middlewares/centralized-error-handler.js
@@ -1,4 +1,4 @@
-module.exports = (err, req, res, next) => {
+module.exports.CentralizedErrorHandler = (err, req, res, next) => {
   res
     .status(err.statusCode)
     .send({
--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -1,10 +1,8 @@
 const rateLimit = require('express-rate-limit');
 
-module.exports.limiter = rateLimit({
+const limiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 минут
   max: 100, // ограничение в 100 запросов с каждого IP адреса
-  message: {
-    status: 429,
-    error: 'Вы превысили допустимое количество запросов.',
-  },
 });
+
+module.exports = limiter;
