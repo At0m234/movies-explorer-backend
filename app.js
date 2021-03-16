56ad50c918b74519f009a38a67a2a83ac57f3ae4
At0m234 <61670155+At0m234@users.noreply.github.com>
2021-03-16 14:55:13 +0300

    stage 2 backend final

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
-const cors = require('cors');
+// const cors = require('cors');
 const { celebrate, Joi, errors } = require('celebrate');
 const { limiter } = require('./middlewares/limiter');
 
@@ -51,27 +51,27 @@ mongoose.connect(MOV_EXP_DB, {
 //   next();
 // });
 
-const allowedCors = [
-  'http://api.movexp.students.nomoredomains.icu',
-  'https://api.movexp.students.nomoredomains.icu',
-  'http://movexp.students.nomoredomains.icu',
-  'https://movexp.students.nomoredomains.icu',
-  'http://localhost:3000',
-];
+// const allowedCors = [
+//   'http://api.movexp.students.nomoredomains.icu',
+//   'https://api.movexp.students.nomoredomains.icu',
+//   'http://movexp.students.nomoredomains.icu',
+//   'https://movexp.students.nomoredomains.icu',
+//   'http://localhost:3000',
+// ];
 
-app.use(cors());
+// app.use(cors());
 
-app.use((req, res, next) => {
-  const { origin } = req.headers;
+// app.use((req, res, next) => {
+//   const { origin } = req.headers;
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//   }
 
-  next();
-});
+//   next();
+// });
 
-app.options('*', cors());
+// app.options('*', cors());
 
 // app.use(cors({
 //   'Access-Control-Allow-Origin': 'https://movexp.students.nomoredomains.icu',
